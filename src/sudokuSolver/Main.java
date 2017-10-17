1fd579363b87daf0520a08dd2e1f6d4ee65ee28c
Soham Roy <soham3.1415@gmail.com>
2017-10-16 23:08:30 -0500

    Code to open blank window

--- src/sudokuSolver/Main.java
+++ src/sudokuSolver/Main.java
@@ -3,6 +3,7 @@ package sudokuSolver;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,10 +12,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("GUI.fxml"));
-			Scene scene = new Scene(root,400,400);
+			GridPane root = new GridPane();
+			Scene scene = new Scene(root,900,900);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Sudoku Solver");
+			primaryStage.getIcons().add(new Image(this.getClass().getClassLoader().getResourceAsStream("sudokuSolver/Icon.png")));
+			primaryStage.setMinWidth(400);
+			primaryStage.setMinHeight(400);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
