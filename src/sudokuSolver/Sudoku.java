0bb96e30b0f0edf0961f7b1ebc723667f391ad1d
Soham Roy <soham3.1415@gmail.com>
2017-11-20 19:17:27 -0600

    Refresh .gitignore

---
+++ src/sudokuSolver/Sudoku.java
@@ -0,0 +1,125 @@
+package sudokuSolver;
+
+import java.util.ArrayList;
+
+public class Sudoku {
+
+	private NumberField[][] numberFields = new NumberField[9][9];
+	
+	public Sudoku (NumberField[][] numberFields) {
+		this.numberFields = numberFields;
+	}
+	
+	public ArrayList<Integer> setPossibleValues(int r, int c) {
+		ArrayList<Integer> possibleValues = new ArrayList<Integer>();
+		for(int i = 1; i <= 9; i++)
+			if(isUsable(i, r, c))
+				possibleValues.add(i);
+		numberFields[r][c].setPossibleValues(possibleValues);
+		return possibleValues ;
+	}
+	
+	public boolean hasNextCell(int r, int c) {
+		r = getNextRValue(r,c);
+		c = getNextCValue(r,c);
+		if(r >= 0 && r < numberFields.length && c >= 0 && c < numberFields[r].length)
+			return true;
+		return false;
+	}
+	
+	public int getNextRValue(int r, int c) {
+		if(c == numberFields[r].length - 1)
+			return r+1;
+		return r;
+	}
+	
+	public int getNextCValue(int r, int c) {
+		if(c == numberFields[r].length - 1)
+			return 0;
+		return c+1;
+	}
+	
+	public boolean hasPreviousCell(int r, int c) {
+		if(r == 0 && c == 0)
+			return false;
+		return true;
+	}
+	
+	public int getPreviousRValue(int r, int c) {
+		if(c == 0)
+			return r-11;
+		return r;
+	}
+	
+	public int getPreviousCValue(int r, int c) {
+		if(c == 0)
+			return numberFields[r].length-1;
+		return c-1;
+	}
+	
+	public boolean isUsable (int value, int r, int c) {
+		if(isInRow(value, r))
+			return false;
+		if(isInColumn(value, c))
+			return false;
+		if(isInSection(value, r, c))
+			return false;
+		return true;
+	}
+	
+	public boolean isInRow(int value, int r) {
+		for(NumberField field : numberFields[r])
+			if(field.getValue() == value)
+				return true;
+		return false;
+	}
+	
+	public boolean isInColumn(int value, int c) {
+		for(NumberField field[] : numberFields)
+			if(field[c].getValue() == value)
+				return true;
+		return false;
+	}
+	
+	public boolean isInSection(int value, int r, int c) {
+		return isInSection(value, getSectionNumber(r, c));
+	}
+	
+	public boolean isInSection(int value, int section) {
+		NumberField[][] sectorFields = getSection(section);
+		for(NumberField[] row : sectorFields)
+			for(NumberField field : row)
+				if(field.getValue() == value)
+					return true;
+		return false;
+	}
+	
+	public NumberField[][] getSection(int r, int c) {
+		return getSection(getSectionNumber(r, c));
+	}
+	
+	public NumberField[][] getSection(int section) {
+		NumberField[][] numberFieldsSection = new NumberField[3][3];
+		int startRowPos = ((section-1)/3)*3;
+		int startColPos = ((section-1)%3)*3;
+		for(int r = startRowPos; r < startRowPos+3; r++)
+			for(int c = startColPos; c < startColPos+3; c++)
+				numberFieldsSection[r%3][c%3] = numberFields[r][c];
+		return numberFieldsSection;
+	}
+	
+	public int getSectionNumber(int r, int c) {
+		return (((r/3)*3)+1) + c/3;
+	}
+	
+	public String toString() {
+		String output = "";
+		for(NumberField[] row : numberFields) {
+			for(NumberField field : row)
+				output += field.getText() + " ";
+			output += "\n";
+		}
+		output += "\b";
+		return output;
+	}
+}
